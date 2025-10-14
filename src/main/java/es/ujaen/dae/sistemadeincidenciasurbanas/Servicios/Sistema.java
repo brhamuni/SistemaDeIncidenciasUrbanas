aa432b2f4a5d350e2c7bfd57c772ad7ad554bd12
ims00051 <ims00051@red.ujaen.es>
2025-10-14 22:04:26 +0200

    Creacion de la clase Sistema

--- src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/Servicios/Sistema.java
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/Servicios/Sistema.java
@@ -1,4 +1,25 @@
 package es.ujaen.dae.sistemadeincidenciasurbanas.Servicios;
 
+import es.ujaen.dae.sistemadeincidenciasurbanas.entidades.Incidencia;
+import es.ujaen.dae.sistemadeincidenciasurbanas.entidades.TipoIncidencia;
+import es.ujaen.dae.sistemadeincidenciasurbanas.entidades.Usuario;
+
+import java.util.List;
+
 public class Sistema {
+
+    private List<Usuario> usuarios;
+    private List<Incidencia> incidencias;
+    private List<TipoIncidencia> tiposDeIncidencia;
+    private Usuario usuarioActual;
+
+    public List<Usuario> getUsuarios() { return usuarios; }
+    public List<Incidencia> getIncidencias() { return incidencias; }
+    public List<TipoIncidencia> getTiposDeIncidencia() { return tiposDeIncidencia;}
+    public Usuario getUsuarioActual() { return usuarioActual; }
+
+    public void setUsuarios(List<Usuario> usuarios) { this.usuarios = usuarios; }
+    public void setIncidencias(List<Incidencia> incidencias) { this.incidencias = incidencias; }
+    public void setTiposDeIncidencia(List<TipoIncidencia> tiposDeIncidencia) { this.tiposDeIncidencia = tiposDeIncidencia; }
+    public void setUsuarioActual(Usuario usuarioActual) { this.usuarioActual = usuarioActual; }
 }
