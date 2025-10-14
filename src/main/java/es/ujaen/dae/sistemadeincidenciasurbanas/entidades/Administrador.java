391e77818ce8dcf88d0b906e6065adce2f20c6e4
agh00040 <brham.uni@gmai.com>
2025-10-14 17:27:43 +0200

    Clase Administrador

---
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/entidades/Administrador.java
@@ -0,0 +1,18 @@
+package es.ujaen.dae.sistemadeincidenciasurbanas.entidades;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+@DiscriminatorValue("admin")
+public class Administrador extends Usuario {
+
+    public Administrador() {
+        super();
+    }
+
+    public Administrador(String apellidos, String nombre, LocalDate fechaNacimiento, String direccion, String telefono, String email, String login, String claveAcceso) {
+        super(apellidos, nombre, fechaNacimiento, direccion, telefono, email, login, claveAcceso);
+    }
+}
