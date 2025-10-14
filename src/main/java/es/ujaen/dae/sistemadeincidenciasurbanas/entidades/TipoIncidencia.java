9f65e958b93964891d42819fdf5d439761586f5d
agh00040 <brham.uni@gmai.com>
2025-10-14 17:03:32 +0200

    TipoIncidencia actualizado

--- src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/entidades/TipoIncidencia.java
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/entidades/TipoIncidencia.java
@@ -1,6 +1,79 @@
 package es.ujaen.dae.sistemadeincidenciasurbanas.entidades;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
 public class TipoIncidencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
+
+    @NotBlank(message = "La descripción не puede estar vacía")
     private String descripcion;
+
+    @OneToMany(mappedBy = "tipoIncidencia")
+    private List<Incidencia> incidencias;
+
+    public TipoIncidencia() {
+        this.incidencias = new ArrayList<>();
+    }
+
+    public TipoIncidencia(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.incidencias = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Incidencia> getIncidencias() {
+        return incidencias;
+    }
+
+    public void setIncidencias(List<Incidencia> incidencias) {
+        this.incidencias = incidencias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipoIncidencia that = (TipoIncidencia) o;
+        return nombre.equals(that.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
