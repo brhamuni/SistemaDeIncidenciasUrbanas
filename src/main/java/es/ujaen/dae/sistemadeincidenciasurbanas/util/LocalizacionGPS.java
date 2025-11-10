483f651a626996a7882900d472e9794ddc9c85ac
ard00032 <ard00032@red.ujaen.es>
2025-11-10 20:47:35 +0100

    LocalizacionGPS marcado Embeddable para JPA

--- src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/util/LocalizacionGPS.java
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/util/LocalizacionGPS.java
@@ -1,8 +1,17 @@
 package es.ujaen.dae.sistemadeincidenciasurbanas.util;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Column;
+
+@Embeddable
 public class LocalizacionGPS {
+    @Column(name = "gps_x")
     private int x;
+
+    @Column(name = "gps_y")
     private int y;
+
+    public LocalizacionGPS() {}
 
     public LocalizacionGPS(int x, int y) {
         this.x = x;
