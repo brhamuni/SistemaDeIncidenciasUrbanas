5588abf6a1ed12482a64da48e82924e8d129dee9
agh00040 <agh00040@red.ujaen.es>
2025-12-11 19:45:47 +0100

    Inicio Practica 3, creacion clases DTO, REST Y Seguridad

---
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/rest/dto/CrearIncidenciaDTO.java
@@ -0,0 +1,4 @@
+package es.ujaen.dae.sistemadeincidenciasurbanas.rest.dto;
+
+public class CrearIncidenciaDTO {
+}
---
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/seguridad/ServicioSeguridad.java
@@ -0,0 +1,4 @@
+package es.ujaen.dae.sistemadeincidenciasurbanas.seguridad;
+
+public class ServicioSeguridad {
+}
