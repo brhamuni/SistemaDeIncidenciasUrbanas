50f70163ae4931617d54477cbaf3bbaac56591c6
ims00051 <ims00051@red.ujaen.es>
2025-12-12 12:40:53 +0100

    Creación de la clase FiltroAutenticacionJwt

---
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/seguridad/FiltroAutenticacionJwt.java
@@ -0,0 +1,45 @@
+package es.ujaen.dae.sistemadeincidenciasurbanas.seguridad;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class FiltroAutenticacionJwt extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader(AUTHORIZATION);
+
+        if (header != null && header.startsWith("Bearer ")) {
+            Claims claims = null;
+
+            try {
+                claims = UtilJwt.extraerContenido(header.substring(7));
+            } catch (JwtException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            var authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(claims.get("roles", String.class));
+
+            var authToken = new UsernamePasswordAuthenticationToken(
+                    claims.getSubject(),
+                    null,
+                    authorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
