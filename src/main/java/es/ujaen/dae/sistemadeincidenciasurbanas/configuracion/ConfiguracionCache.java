f553f039d02d4dbe11bfcd13b2dac17d4ab85a0c
ard00032 <ard00032@red.ujaen.es>
2025-12-15 02:03:16 +0100

    Configuración de Caché

---
+++ src/main/java/es/ujaen/dae/sistemadeincidenciasurbanas/configuracion/ConfiguracionCache.java
@@ -0,0 +1,9 @@
+package es.ujaen.dae.sistemadeincidenciasurbanas.configuracion;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class ConfiguracionCache {
+}
